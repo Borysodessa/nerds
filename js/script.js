5ee7ced153518ae6f9a4702f186f91fef9aaf740
Borysodessa / <chayka2000@mail.ru>
2020-12-18 13:01:16 +0300

    adapt

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 const feedbackButton = document.querySelector('.contact__button');
 const modalForm = document.querySelector('.popup__section');
 const closePopup = document.querySelector('.close-popup__wrap');
+const login = modalForm.querySelector('[name = name]');
+
 
 feedbackButton.addEventListener('click', function (evt) {
   evt.preventDefault(evt);
   modalForm.classList.add('display');
+  login.focus();
 });
 
 closePopup.addEventListener('click', function(evt) {
   evt.preventDefault(evt);
   modalForm.classList.remove('display');
+});
+
+
+$('.slider__list').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
 });
